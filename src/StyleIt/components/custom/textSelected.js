86dc9f238604462ef4bf0fe76670f8bb9021e6e8
webizzart <webizzart@gmail.com>
2020-08-10 22:57:11 +0300

    add empty char

--- src/StyleIt/components/custom/textSelected.js
+++ src/StyleIt/components/custom/textSelected.js
@@ -3,5 +3,8 @@ class TextSelection extends HTMLElement {
     constructor(props) {
       super();
     }
+    connectedCallback(){
+      this.innerHTML = "&#8203;";
+    }
   }
   customElements.define('text-selection', TextSelection); 
