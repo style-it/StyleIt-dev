4901bbe87dedefe28beaf39bf8b6dbf1c4ab0244
webizzart <webizzart@gmail.com>
2020-08-14 15:09:09 +0300

    fix underline rule

--- src/StyleIt/rules/rules.js
+++ src/StyleIt/rules/rules.js
@@ -1,12 +1,17 @@
 import { setStyle } from "../services/style.service";
+import { splitHTML } from "../utilis/splitHTML";
 
 //TODO:review
+//question: should i add all the rules in one array or dispatch by css keys..
 export function UseRules (data) {
     const ColorTextDecorationRule = (data) =>{
         if (data.key === "color") {
             const textDecoration = data.element.closest(`[style*='text-decoration']`);
             if (textDecoration) {
-                setStyle(textDecoration, 'text-decoration-color', data.value);
+               const splits =  splitHTML(data.element,textDecoration);
+               if(splits) {
+                setStyle(splits.center, 'text-decoration',`${textDecoration.style.textDecoration} ${data.value}`);
+               }
             }
         }
     }
