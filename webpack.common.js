b813e884cd0667e3067cd5563f15baff9927b2e8
webizzart <webizzart@gmail.com>
2020-08-30 20:16:21 +0300

    fix library name

--- webpack.common.js
+++ webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
     entry: `${__dirname}/index.js`,
     output: {
         filename: `${libraryName}.js`,
-        library: libraryName,
+        library: 'styleit',
         libraryTarget: libraryTarget || 'umd',
         globalObject: '(typeof self !== \'undefined\' ? self : this)', // TODO Hack (for Webpack 4+) to enable create UMD build which can be required by Node without throwing error for window being undefined (https://github.com/webpack/webpack/issues/6522)
         umdNamedDefine: true
