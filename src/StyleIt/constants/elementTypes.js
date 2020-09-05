150dadc4c7579d338657563909b63ccd43c5ac0c
webizzart <webizzart@gmail.com>
2020-09-05 11:54:05 +0300

    start create a blocktypes

--- src/StyleIt/constants/elementTypes.js
+++ src/StyleIt/constants/elementTypes.js
@@ -1,18 +1,41 @@
-export const Types = {
-    '#text': '#text',
-    'P': 'P',
-    'SPAN': 'SPAN',
-    'A': 'A',
-    'DIV': 'DIV',
-    'BR': 'BR',
-    'CODE': 'CODE',
-    'PRE': 'PRE',
-    'H1': 'H1',
-    'H2': 'H2',
-    'H3': 'H3',
-    'H4': 'H4',
-    'H5': 'H5',
-    'H6': 'H6',
-    'IMG': 'IMG',
-    'TABLE': 'TABLE'
+
+export const TYPESBLOCK = {
+    "P": "P",
+    "DIV": "DIV",
+    "PRE": "PRE",
+    "H1": "H1",
+    "H2": "H2",
+    "H3": "H3",
+    "H4": "H4",
+    "H5": "H5",
+    "H6": "H6",
+    "ADDRESS": "ADDRESS",
+    "TD":"TD",
+    "TH":"TH",
+    "SECTION": "SECTION",
+    "DT": "DT",
+    "ARTICLE": "ARTICLE",
+    "FORM": "FORM",
+    "UL": "UL",
+    "LI": "LI",
+    "OL": "OL",
+    "DL": "DL",
+    "MAIN": "MAIN",
+    "BLOCKQUOTE ":"BLOCKQUOTE",
+    "NAV": "NAV"
+}
+
+export const TYPES = {
+    "#text": "#text",
+    "P": "P",
+    "SPAN": "SPAN",
+    "A": "A",
+    "ADDRESS": "ADDRESS",
+    "DIV": "DIV",
+    "BR": "BR",
+    "CODE": "CODE",
+    "PRE": "PRE",
+    "IMG": "IMG",
+    "TABLE": "TABLE",
+    "HR": "HR"
 }
