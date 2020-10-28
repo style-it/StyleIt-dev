084ad787c528651c336ca16ed21c4875beb203d3
webizzart <webizzart@gmail.com>
2020-10-28 21:06:28 +0200

    add keyPress plugin

---
+++ src/StyleIt/services/keyPress/KeyPress.js
@@ -0,0 +1,38 @@
+
+
+export default class KeyPress {
+    constructor(target, keys) {
+
+        if (!target) {
+            console.error('[-] keyPress => target is null');
+            return null;
+        }
+        if (!Array.isArray(keys)) {
+            console.error("[-] keyPress => keys must be an array");
+            return null;
+        }
+        this.keys = keys;
+        this.target = target;
+
+
+        this.keypress = (e) => {
+            if (e.ctrlKey && e.key !== "Control") {
+                this.keys.forEach(key => {
+                    if (Array.isArray(key) && key.length === 2 && key[0] === e.key) {
+                        e.preventDefault();
+                        key[1]();
+                    }
+                })
+            }
+
+        }
+
+
+        this.target.addEventListener('keydown', this.keypress);
+
+        this.Destroy = () => {
+            this.target.removeEventListener('keydown', this.keypress);
+            this.target = null;
+        }
+    }
+}
