c9eb7d6e8ca73d930d67f6aeb8e1103b7d72ded8
style-it <styleit.api@gmail.com>
2020-11-30 21:17:15 +0200

    remove unnececery imports

--- src/StyleIt/services/copyPaste/copyPaste.service.js
+++ src/StyleIt/services/copyPaste/copyPaste.service.js
@@ -1,10 +1,5 @@
 
-import { wrapRangeWithElement, setSelectionFlags, setSelectionBetweenTwoNodes, pasteHtmlAtCaret} from '../range.service';
-import { normalizeElement } from '../textEditor.service';
-import { getInheirtCss, setStyles, getStyle, normalizeStyle } from '../style.service';
-// import { elementToJson } from '../elements.service';
-import { splitHTML } from '../../utilis/splitHTML';
-
+import { pasteHtmlAtCaret} from '../range.service';
 //TODO:review
 //should we use the clipboard as string ? or pass in other way as json ? 
 export default class CopyPaste {
@@ -17,7 +12,6 @@ export default class CopyPaste {
         }
         this.target = target;
         this.onPaste = (event) =>{
-            debugger
             function walkTheDOM(node, func) {
                 func(node);
                 node = node.firstChild;
@@ -46,13 +40,7 @@ export default class CopyPaste {
                 const nodes = [];
                 // Example usage: Process all Text nodes on the page
                 const notValid = ["META", "LINK", "SCRIPT", "TCXSPAN", "O:P", "STYLE"];
-                // walkTheDOM(temp, function (node) {
-                //   if(node.nodeType === 8) {
-                //     node.remove();
-                //   }else if(node.nodeName ==="STYLE"){
-                //     node.remove();
-                //   }
-                // });
+              
                 walkTheDOM(temp, function (node) {
                   if (!notValid.includes(node.nodeName)) {
                     nodes.push(node);
@@ -92,82 +80,7 @@ export default class CopyPaste {
                 pasteHtmlAtCaret(rendered);
                   rendered.remove();
               }
-        } 
-        // this.onPaste = (e) => {
-        //     const stringToHTML = function (str) {
-        //         const parser = new DOMParser();
-        //         const doc = parser.parseFromString(str, 'text/html');
-        //         return doc.body;
-        //     };
-        //     const clip = e.clipboardData.getData('text/html');
-        //     const markup = stringToHTML(clip);
-        //     const isLocalElement = markup.querySelector(`.${this.uniqeID}`);
-        //     if (isLocalElement) {
-        //         pasteHtmlAtCaret(isLocalElement.outerHTML);
-        //         let pastedElement = this.target.querySelector(`.${this.uniqeID}`);
-        //         if (pastedElement) {
-        //             Array.from(pastedElement.querySelectorAll('span')).forEach(child => {
-        //                 const style = getStyle(child);
-        //                 normalizeStyle(child);
-        //                 for (const key in style) {
-        //                     if (style.hasOwnProperty(key)) {
-        //                         const foundedEl = child.parentElement.closest(`[style*='${key}']`);
-        //                         if (foundedEl) {
-        //                             splitHTML(child, foundedEl);
-        //                         }
-        //                     }
-        //                 }
-        //             });
-        //             Array.from(this.target.querySelectorAll(`.${this.uniqeID}`)).forEach(flag => flag.classList.remove(this.uniqeID));
-        //             normalizeElement(this.target);
-        //         }
-
-        //     }
-        //     else {
-        //         let paste = (e.clipboardData || window.clipboardData).getData('text');
-        //         const selection = window.getSelection();
-        //         if (!selection.rangeCount) return false;
-        //         selection.deleteFromDocument();
-        //         selection.getRangeAt(0).insertNode(document.createTextNode(paste));
-        //     }
-        //     e.preventDefault();
-        // }
-        // this.onCopy = (e) => {
-        //     //review: what should we can do here... it always array of one element..
-        //     const ranges = wrapRangeWithElement();
-        //     if(ranges.length === 0){
-        //         console.warn('no selected elements..');
-        //     }
-        //     // set flags to keep text selection..
-        //     let nodes = ranges.map(el => {
-        //         //clone the nodes and unwrap the wrapped textnodes on the dom..
-        //         const collectStyle = getInheirtCss(el, this.target);
-        //         setStyles(el, collectStyle);
-        //         return el.cloneNode(true);
-        //     });
-            
-        //     const { firstFlag, lastFlag } = setSelectionFlags(ranges[0], ranges[ranges.length - 1]);
-        //     ranges.forEach(we => we.unwrap());
-            
-        //     setSelectionBetweenTwoNodes(firstFlag, lastFlag);
-        //     this.target.normalize();
-
-        //     const element = document.createElement('span');
-
-        //     nodes = nodes.reduce(function(filtered, node) {
-        //         //TODO: filtered by the valid tags..
-        //         if (node.nodeName === "SPAN") {
-        //            filtered.push(node.outerHTML);
-        //         }
-        //         return filtered;
-        //       }, []);
-        //       element.innerHTML = nodes.join("");
-        //     element.className = this.uniqeID;
-        //     // this.savedJson = elementToJson(element);
-        //     e.clipboardData.setData('text/html', element.outerHTML);
-        //     e.preventDefault();
-        // }
-
+        }      
         this.target.addEventListener('paste', this.onPaste);
         this.target.addEventListener('copy', this.onCopy);
     }
