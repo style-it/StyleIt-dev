a4771bb0f3ed3570ad3ccb94d705e172bb8df9b4
style-it <styleit.api@gmail.com>
2021-01-11 23:00:08 +0200

    implement copy paste with styles

--- src/StyleIt/services/copyPaste/copyPaste.service.js
+++ src/StyleIt/services/copyPaste/copyPaste.service.js
@@ -1,97 +1,108 @@
 
-import { EVENTS } from '../events/events';
-//TODO:review
-//should we use the clipboard as string ? or pass in other way as json ? 
-export default class CopyPaste {
-  constructor(target) {
-    this.uniqeID = 'styleit-copy-paste-on-action';
-    if (!target) {
-      //TODO: use the validator
-      console.error('[-] Copy Paste => target is null');
-      return null;
+const validStyles = {
+  "textDecoration": true,
+  "fontSize": true,
+  "fontStyle": true,
+  // "lineHeight": true,
+  "color": true,
+  "backgroundColor": true,
+  "fontWeight": true,
+  "textShadow": true,
+  "fontFamily": true
+
+}
+const onPaste = (event) => {
+  document.execCommand("DefaultParagraphSeparator", false, "p");
+  event.preventDefault();
+  let textContentContainer;
+  let HtmlContainer;
+  try {
+    const data = event.clipboardData || window.clipboardData;
+    textContentContainer = document.createElement("div");
+    textContentContainer.innerText = data.getData('text/plain').trim();;
+    HtmlContainer = document.createElement("div");
+    HtmlContainer.innerHTML = data.getData('text/html').trim();
+    document.body.appendChild(textContentContainer);
+    document.body.appendChild(HtmlContainer);
+
+    const htmlNodes = [];
+    const recurse = (el, callback) => {
+      el.childNodes.forEach(node => {
+        callback(node);
+        if (node.nodeType === 1) {
+          recurse(node, callback);
+        }
+      })
+
     }
-    this.target = target;
-    this.onPaste = (event) => {
-      document.execCommand("defaultParagraphSeparator", false, "p");
-      function walkTheDOM(node, func) {
-        func(node);
-        node = node.firstChild;
-        while (node) {
-          walkTheDOM(node, func);
-          node = node.nextSibling;
-        }
-      }
 
-      const target = event.target;
-      const block = target.closest(`#${this.target.id}`);
-      if (block) {
-        const data = event.clipboardData || window.clipboardData;
-        let clip = data.getData('text/html').trim();
-        if (!clip) {
-          clip = data.getData('text/plain').trim();
-          clip = clip.replace(/\n/g, "<br/>")
-        }
-        //TODO: regex all togheter!
-        let markup = clip;
-        ["<html>", "<body>", "<!--StartFragment-->", "<!--EndFragment-->", "</body>", "</html>"].
-          forEach(rep => markup = markup.replace(rep, ""));
-        const temp = document.createElement("div");
-        temp.innerHTML = markup.replace(/<!--([\s\S]*?)-->/g, "");
 
-        const nodes = [];
-        // Example usage: Process all Text nodes on the page
-        const notValid = ["META", "LINK", "SCRIPT", "TCXSPAN", "O:P", "STYLE"];
 
-        walkTheDOM(temp, function (node) {
-          if (!notValid.includes(node.nodeName)) {
-            nodes.push(node);
+    recurse(HtmlContainer, (node) => {
+      if (node.nodeType === 3) {
+        const span = document.createElement("span");
+        for (const key in validStyles) {
+          if (Object.hasOwnProperty.call(validStyles, key)) {
+            span.style[key] = "inherit";
           }
-        });
+        }
+        node.wrap(span);
+        htmlNodes.push(span);
+      }
+    });
+    htmlNodes.forEach(s => {
+      if (!s.textContent.trim()) return;
+      const collectedStyles = window.getComputedStyle(s);
+      for (const key in collectedStyles) {
+        if (Object.hasOwnProperty.call(collectedStyles, key)) {
+          const value = collectedStyles[key];
+          if (validStyles[key]) {
+            if (value === "inherit") {
+              s.style[key] = "";
+            } else if (value.includes("none")) {
+              s.style[key] = "";
+            } else if (value.replace(/ /gm, "").includes("(0,0,0")) {
+              s.style[key] = "";
+            } else if (value === "normal") {
+              s.style[key] = "";
+            } else {
+              s.style[key] = value;
 
-        const rendered = document.createElement("div");
-        document.body.appendChild(rendered);
-
-        nodes.forEach((newEl, index) => {
-          rendered.appendChild(newEl);
-          if (newEl.textContent && newEl.nodeType === 1 && newEl.nodeName !== "br") {
-            const computed = window.getComputedStyle(newEl);
-            if (computed.display === "block") {
-              const br = document.createElement("br");
-              newEl.insertAfter(br);
             }
           }
-          if (newEl.nodeType !== 3 && newEl.nodeName !== "BR") {
-            newEl.remove();
-          }
-        });
-        const arr = Array.from(rendered.childNodes);
-        for (let index = 0; index < arr.length; index++) {
-          const element = arr[index];
-          if (!element.textContent.trim()) {
-            element.remove();
-          } else {
-            if (!arr[arr.length - 1].textContent.trim()) {
-              arr[arr.length - 1].remove();
-            }
-            break;
-          }
-        }
-        event.preventDefault();
 
-        document.execCommand("insertHTML", false, rendered.innerHTML);
-        // pasteHtmlAtCaret(rendered);
-        rendered.remove();
-        if (typeof (EVENTS["paste"]) === "function") {
-          EVENTS["paste"](rendered.innerHTML);
         }
       }
-    }
-    this.target.addEventListener('paste', this.onPaste);
-    this.target.addEventListener('copy', this.onCopy);
+    });
+    const textNodes = []
+    recurse(textContentContainer, (node => {
+      textNodes.push(node)
+    }));
+    htmlNodes.forEach(el => {
+      const replaced = textContentContainer.innerHTML.replace(el.textContent, el.outerHTML);;
+      textContentContainer.innerHTML = replaced
+    });
+    document.execCommand('inserthtml', false, textContentContainer.innerHTML);
   }
-  Destroy() {
-    this.target.removeEventListener('paste', this.onPaste);
-    this.target.removeEventListener('copy', this.onCopy);
-    this.target = null;
+  catch (error) {
+    console.error(error);
+  }
+  finally {
+    textContentContainer.parentNode.removeChild(textContentContainer);
+    HtmlContainer.parentNode.removeChild(HtmlContainer);
+  }
+};
+
+export default class CopyPaste {
+
+  constructor(target) {
+    this.target = target;
+    this.start();
+  }
+  start() {
+    this.target.addEventListener("paste", onPaste);
+  }
+  destroy() {
+    this.target.removeEventListener("paste", onPaste);
   }
 }
