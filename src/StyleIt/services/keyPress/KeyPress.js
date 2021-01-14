71566a9a49fe13365f22b2316d5acc4bcb1c7a45
style-it <styleit.api@gmail.com>
2021-01-15 00:16:57 +0200

    handle breakline

--- src/StyleIt/services/keyPress/KeyPress.js
+++ src/StyleIt/services/keyPress/KeyPress.js
@@ -1,37 +1,63 @@
+import { GetClosestBlockElement, insertAfter, pasteHtmlAtCaret } from "../range.service";
 
 
 export default class KeyPress {
 
-    constructor(target, keys) {
-
+    constructor(target, options) {
         if (!target) {
             console.error('[-] keyPress => target is null');
             return null;
         }
-        if (!Array.isArray(keys)) {
-            console.error("[-] keyPress => keys must be an array");
-            return null;
-        }
-        this.keys = keys;
         this.target = target;
 
 
         this.keypress = (e) => {
-            const ctrl = "^";
-            const alt = "!";
-            const shift  = "+";
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                if(e.shiftKey){
+                    pasteHtmlAtCaret("<br/>")
+                    // document.execCommand('inserthtml', false, );
+                    return false;
+                }
+                document.execCommand('inserttext', false, "\n");
+                return false;
+                // const range = document.createRange();
+                // const selection = window.getSelection();
+                // e.preventDefault();
+                // var blockElement = GetClosestBlockElement(selection.anchorNode);
+                // range.setStart(selection.focusNode, selection.baseOffset);
+                // range.setEnd(blockElement,blockElement.childNodes.length);
+                // selection.removeAllRanges();
+                // selection.addRange(range);
+                // const docFragment = range.extractContents();
+                // Array.from(docFragment.children).forEach(child => {
+                //     if (child.nodeType === 1 && !child.textContent.trim()) {
+                //         child.unwrap();
+                //     }
+                // });
+              
+                // const el = document.createElement(blockElement.nodeName);
+                // el.appendChild(docFragment);
+                // insertAfter(el, blockElement);
+                // selection.removeAllRanges();
 
-            /*
-            ["^+66",() => {}];
-            */
-            if (e.ctrlKey) {
-                this.keys.forEach(key => {
-                    if (Array.isArray(key) && key.length === 2 && key[0] === e.keyCode && typeof(key[1]) === "function") {
-                        e.preventDefault();
-                        key[1]();
-                    }
-                })
             }
+
+            // const ctrl = "^";
+            // const alt = "!";
+            // const shift  = "+";
+
+            // /*
+            // ["^+66",() => {}];
+            // */
+            // if (e.ctrlKey) {
+            //     this.keys.forEach(key => {
+            //         if (Array.isArray(key) && key.length === 2 && key[0] === e.keyCode && typeof(key[1]) === "function") {
+            //             e.preventDefault();
+            //             key[1]();
+            //         }
+            //     })
+            // }
 
         }
 
