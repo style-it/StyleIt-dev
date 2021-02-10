584992647d6b2505c8ab9336664641c6b938181f
style-it <styleit.api@gmail.com>
2021-02-10 23:37:43 +0200

    update copy paste

--- src/StyleIt/services/copyPaste/copyPaste.service.js
+++ src/StyleIt/services/copyPaste/copyPaste.service.js
@@ -1,10 +1,9 @@
 import { block_elments } from "../../constants/block_elms";
 import { void_elements } from "../../constants/void_elms";
 import { splitHTML } from "../../utilis/splitHTML";
-import { walkOnElement, walkTheDOM, wrapNakedTextNodes } from "../elements.service";
-import { pasteHtmlAtCaret, setCaretAt, GetClosestBlockElement } from "../range.service";
+import { pasteHtmlAtCaret, setCaretAt } from "../range.service";
 import { getInheirtCss, setStyles } from "../style.service";
-import { normalizeElement } from "../textEditor.service";
+import { getCleanText, normalizeElement } from "../textEditor.service";
 
 
 export default class CopyPaste {
@@ -75,7 +74,6 @@ export default class CopyPaste {
     const p = document.createElement("p");
     p.innerHTML = copied;
 
-    // document.execCommand('inserttext', false, content);
     pasteHtmlAtCaret(p);
     setCaretAt(p);
 
@@ -96,29 +94,32 @@ export default class CopyPaste {
     //on copied on the editor localy
     if (copied) {
       const pastedContainer = document.createElement("div");
-      debugger
       pastedContainer.innerHTML = copied;
       pasteHtmlAtCaret(pastedContainer);
-      Array.from(this.target.children).forEach(child=>{
-        if(!child.textContent.trim()){
+      Array.from(pastedContainer.children).forEach(child => {
+        if (!getCleanText(child.textContent) && !void_elements[child.nodeName]) {
           child.parentElement.removeChild(child);
         }
       });
-      Array.from(pastedContainer.children).forEach(child=>{
+      Array.from(pastedContainer.children).forEach(child => {
         const sameNode = child.parentElement.closest("h1,h2,h3,h4,h5,h6,p");
-        if(child.nodeType === 1 && child.nodeName !== "SPAN" && sameNode){
-          child.unwrap();
-
+        if (sameNode && block_elments[child.nodeName] && getCleanText(sameNode.textContent) === getCleanText(pastedContainer.textContent)) {
+          sameNode.parentElement.insertBefore(child, sameNode);
         }
-        if(!child.textContent.trim()){
+        else if (child.nodeType === 1 && block_elments[child.nodeName] && sameNode) {
+          if (pastedContainer.children.length > 1) {
+            child.insertAfter(document.createElement("BR"));
+          }
+          child.unwrap();
+        }
+        else if (!getCleanText(child.textContent)) {
           child.unwrap();
         }
       });
-      debugger
       const block = pastedContainer.closest("h1,h2,h3,h4,h5,h6,p");
-      if(block){
-        const parts = splitHTML(pastedContainer,block,{tag:block.nodeName});
-        if(parts) {
+      if (block) {
+        const parts = splitHTML(pastedContainer, block, { tag: block.nodeName });
+        if (parts) {
           parts.left.appendChild(parts.center);
           parts.center.appendChild(parts.right);
           parts.right.unwrap();
@@ -128,8 +129,6 @@ export default class CopyPaste {
       normalizeElement(this.target);
       setCaretAt(pastedContainer);
       pastedContainer.unwrap();
-
-    
     } else {
       this.pastePlainText(event);
     }
