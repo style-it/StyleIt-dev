8350419642c8be9dbeeef850e0b61746e9bdab6e
style-it <styleit.api@gmail.com>
2021-02-18 22:59:10 +0200

    export inline elems as array

--- src/StyleIt/constants/inline_elems.js
+++ src/StyleIt/constants/inline_elems.js
@@ -1,3 +1,3 @@
-const inline_elemets_arr = ["SPAN","BOLD","STRONG","EM","I","S","MARK","B","SUB","SUP","SMALL","INS"];
+export const inline_elemets_arr = ["SPAN","BOLD","STRONG","EM","I","S","MARK","B","SUB","SUP","SMALL","INS"];
 export const inline_elements = {};
 inline_elemets_arr.forEach(tag=>{inline_elements[tag] = tag});
