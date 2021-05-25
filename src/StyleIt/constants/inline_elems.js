5c7ea6fad6130e55381c7942e128b171d810f9ba
style-it <styleit.api@gmail.com>
2021-05-25 08:46:42 +0300

    add a tag into the inline_elems

--- src/StyleIt/constants/inline_elems.js
+++ src/StyleIt/constants/inline_elems.js
@@ -1,3 +1,3 @@
-export const inline_elemets_arr = ["SPAN","BOLD","STRONG","EM","I","S","MARK","B","SUB","SUP","SMALL","INS"];
+export const inline_elemets_arr = ["SPAN","BOLD","STRONG","EM","I","S","MARK","B","SUB","SUP","SMALL","INS","A"];
 export const inline_elements = {};
 inline_elemets_arr.forEach(tag=>{inline_elements[tag] = tag});
