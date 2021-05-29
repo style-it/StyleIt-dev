2194415102eb13bc4bbacf49a9888edabbff1bd9
style-it <styleit.api@gmail.com>
2021-05-29 11:30:07 +0300

    wrapwith to toggleTag

--- src/StyleIt/index.js
+++ src/StyleIt/index.js
@@ -116,12 +116,12 @@ export default class Core {
         }
 
     }
-    wrapWith(tagName, options) {
+    toggleTag(tagName, options) {
         if(!tagName){
             return console.warn("tagName not valid");
         }
         if(!inline_elements[tagName.toUpperCase()]){
-            return console.warn("valid tags for wrapWith mehtod: "+inline_elemets_arr.join(","))
+            return console.warn("valid tags for toggleTag mehtod: "+inline_elemets_arr.join(","))
         }
         const selectedElements = querySelectorUnderSelection(tagName);
         let isToggleOn = false;
