45fa1ac0835408ecebbdb1d38475646a9fd4432c
style-it <styleit.api@gmail.com>
2021-06-19 00:17:09 +0300

    lit

--- src/StyleIt/services/Inspector/Inspector.service.js
+++ src/StyleIt/services/Inspector/Inspector.service.js
@@ -1,66 +1,63 @@
-
 import { collectStyleFromSelectedElement } from '../style.service';
 import { EVENTS } from '../events/events';
 import { getSelectedElement } from '../elements.service';
 import { resetURL } from '../link.service';
 
-//TODO: review
-//question: how can we expose the collectedStyle ? 
-//question: should we use this ? https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+// TODO: review
+// question: how can we expose the collectedStyle ?
+// question: should we use this ? https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 export default class Inpsector {
-    constructor(target, onInspect) {
+  constructor(target, onInspect) {
 
-        if (!target) {
-            console.error('[-] Inpsector => target is null');
-            return null;
-        }
-        //TODO: use the validator
-        this.onInspect = onInspect;
-        this.target = target;
-
-        const launchInspect = () => {
-            const collectedStyle = collectStyleFromSelectedElement(this.target);
-            if (typeof (this.onInspect) === "function")
-                this.onInspect(collectedStyle);
-            if (typeof (EVENTS["inspect"]) === "function") {
-                EVENTS["inspect"](collectedStyle);
-            }
-            const selectedElement = getSelectedElement();
-            
-            if(selectedElement ){
-                const aTag = selectedElement.closest("a");
-                if(aTag){
-                    if(typeof(EVENTS["inspectLink"]) === "function"){
-                        EVENTS["inspectLink"]({
-                            protocol:aTag.protocol,
-                            href:resetURL(aTag.href),
-                            target:aTag.getAttribute("target") || "",
-                            element:aTag
-                        });
-                    }
-                }
-            }
-
-        }
-        this.onKeyDown = (e) => {
-            //TODO: review
-            if (e.keyCode === 8 && this.target.children.length <= 1 && !this.target.textContent) {
-                e.preventDefault();
-                return false;
-            }
-            launchInspect();
-        }
-        this.onClick = () => {
-            launchInspect();
-        }
-
-        this.target.addEventListener('click', this.onClick);
-        this.target.addEventListener('keydown', this.onKeyDown);
-
-        this.destroy = () => {
-            this.target.removeEventListener('click', this.onClick);
-            this.target.removeEventListener('keydown', this.onKeyDown);
-            this.target = null;
-        }
+    if (!target) {
+      return null;
     }
+    // TODO: use the validator
+    this.onInspect = onInspect;
+    this.target = target;
+
+    const launchInspect = () => {
+      const collectedStyle = collectStyleFromSelectedElement(this.target);
+      if (typeof this.onInspect === 'function') {this.onInspect(collectedStyle);}
+      if (typeof EVENTS.inspect === 'function') {
+        EVENTS.inspect(collectedStyle);
+      }
+      const selectedElement = getSelectedElement();
+
+      if (selectedElement) {
+        const aTag = selectedElement.closest('a');
+        if (aTag) {
+          if (typeof EVENTS.inspectLink === 'function') {
+            EVENTS.inspectLink({
+              protocol: aTag.protocol,
+              href: resetURL(aTag.href),
+              target: aTag.getAttribute('target') || '',
+              element: aTag
+            });
+          }
+        }
+      }
+
+    };
+    this.onKeyDown = e => {
+      // TODO: review
+      if (e.keyCode === 8 && this.target.children.length <= 1 && !this.target.textContent) {
+        e.preventDefault();
+        return false;
+      }
+      launchInspect();
+    };
+    this.onClick = () => {
+      launchInspect();
+    };
+
+    this.target.addEventListener('click', this.onClick);
+    this.target.addEventListener('keydown', this.onKeyDown);
+
+    this.destroy = () => {
+      this.target.removeEventListener('click', this.onClick);
+      this.target.removeEventListener('keydown', this.onKeyDown);
+      this.target = null;
+    };
+  }
 }
