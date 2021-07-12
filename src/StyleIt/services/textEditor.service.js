aa4b58cbd76e33a14a935d581536ab34f1ed0e6e
style-it <styleit.api@gmail.com>
2021-07-12 16:07:05 +0300

    use with lit

--- src/StyleIt/services/textEditor.service.js
+++ src/StyleIt/services/textEditor.service.js
@@ -1,86 +1,91 @@
-import { normalizeClassName } from "./className.service";
-import { normalizeStyle } from "./style.service";
-import { mergeNodeContect, mergeTwoNodes } from "../utilis/merger";
-import { inline_elements } from "../constants/inline_elems";
+import { normalizeClassName } from './className.service';
+import { normalizeStyle } from './style.service';
+import { mergeNodeContect, mergeTwoNodes } from '../utilis/merger';
+import { inline_elements } from '../constants/inline_elems';
 
-/**
- * @param {Element} el - element to normalize
- */
-export function normalizeElement(el) {
-    if(!el) return;
-    if(!el.isContentEditable){
-        return;
-    }
-    function _normalize(element) {
-        element.normalize();
-        normalizeClassName(element);
-        normalizeStyle(element);
-        if (element && inline_elements[element.nodeName]  && (!element.textContent.trim() || (element.parentElement && element.parentElement.__closest(element.nodeName) && element.attributes.length === 0))) {
-            element.unwrap();
-            return null;
-        }
-        return element;
-    }
-    const recurse = (element) => {
-        Array.from(element.children).forEach((child) => {
-            recurse(child);
-            _normalize(child);
-        })
-        const mergeNodesStyles = element => {
-            let merged = false;
-            Array.from(element.children).forEach((element) => {
-                let wrapper = element;
-                while (wrapper !== null) {
-                    const nextElement = wrapper.nextSibling;
-                    if (!nextElement){
-                        break;
-                    }
-                    wrapper = mergeTwoNodes(wrapper, nextElement);                   
-                    merged = merged || wrapper !== null;
-                }
-            })
-            return merged;
-        }
-
-        const mergeNodesContent = element => {
-            let merged = false;
-            Array.from(element.children).forEach((child) => {
-                let wrapper = child;
-                while (wrapper && wrapper.nodeType === 1) {
-                    wrapper = mergeNodeContect(wrapper);
-                    merged = merged || wrapper !== null;
-                }
-            })
-            return merged;
-        }
-        let mergedStyles = false;
-        let mergedContent = false;
-        do {
-            element = _normalize(element);
-            if(element){
-                mergedStyles = mergeNodesStyles(element);
-                mergedContent = mergeNodesContent(element);
-            }
-           
-        } while (mergedContent && element);
-    }
-        recurse(el);
-}
-
-
-/**
- * @param {Array} textNodes - textnode to remove the zero space like : u200B
- */
-export function removeZeroSpace(textNodes) {
-    if (!Array.isArray(textNodes)) {
-        textNodes = [textNodes];
-    }
-    textNodes.forEach(el => {
-        if (el.nodeType === Node.TEXT_NODE && el.parentElement && el.parentElement.nodeName !== "TEXT-SELECTION") {
-            el.textContent = el.textContent.replace(/\u200B/g, '');
-        }
-    })
+export function isEmptyTextNode(text) {
+  return text.trim().replace(/\s/g, '').replace(/[\u200B-\u200D\uFEFF]/g, '').replace(/\u200f/g, '');
 }
 export function getCleanText(text) {
-    return text.replace(/\s/g, "").replace(/[\u200B-\u200D\uFEFF]/g, '') 
+  return text.replace(/\s/g, '').replace(/[\u200B-\u200D\uFEFF]/g, '');
+}
+
+export function normalizeElement(el) {
+  if (!el) {return;}
+  if (!el.isContentEditable) {
+    return;
+  }
+  function _normalize(element) {
+    element.normalize();
+    normalizeClassName(element);
+    normalizeStyle(element);
+    if (element && inline_elements[element.nodeName] && !isEmptyTextNode(element.textContent)) {
+      element.unwrap();
+      return null;
+    }
+    if (element && element.nodeName === 'SPAN' && element.attributes.length === 0) {
+      element.unwrap();
+      return null;
+    }
+    // if (element && inline_elements[element.nodeName] && element.parentElement && element.parentElement.__closest(element.nodeName)) {
+    //   element.unwrap();
+    //   return null;
+    // }
+    return element;
+  }
+  const recurse = element => {
+    Array.from(element.children).forEach(child => {
+      recurse(child);
+      _normalize(child);
+    });
+    const mergeNodesStyles = _element => {
+      let merged = false;
+      Array.from(_element.children).forEach(child => {
+        let wrapper = child;
+        while (wrapper !== null) {
+          const nextElement = wrapper.nextSibling;
+          if (!nextElement) {
+            break;
+          }
+          wrapper = mergeTwoNodes(wrapper, nextElement);
+          merged = merged || wrapper !== null;
+        }
+      });
+      return merged;
+    };
+
+    const mergeNodesContent = _element => {
+      let merged = false;
+      Array.from(_element.children).forEach(child => {
+        let wrapper = child;
+        while (wrapper && wrapper.nodeType === 1) {
+          wrapper = mergeNodeContect(wrapper);
+          merged = merged || wrapper !== null;
+        }
+      });
+      return merged;
+    };
+    let mergedStyles = false;
+    let mergedContent = false;
+    do {
+      element = _normalize(element);
+      if (element) {
+        mergedStyles = mergeNodesStyles(element);
+        mergedContent = mergeNodesContent(element);
+      }
+
+    } while (mergedContent && element);
+  };
+  recurse(el);
+}
+
+export function removeZeroSpace(textNodes) {
+  if (!Array.isArray(textNodes)) {
+    textNodes = [textNodes];
+  }
+  textNodes.forEach(el => {
+    if (el.nodeType === Node.TEXT_NODE && el.parentElement && el.parentElement.nodeName !== 'TEXT-SELECTION') {
+      el.textContent = el.textContent.replace(/\u200B/g, '');
+    }
+  });
 }
--- src/StyleIt/utilis/merger.js
+++ src/StyleIt/utilis/merger.js
@@ -1,11 +1,9 @@
-import DomUtilis from "./DomUtilis"
-import { getStyle, normalizeStyle } from "../services/style.service";
-import { getClasses, normalizeClassName } from "../services/className.service";
-import { getAttributes } from "../services/attr.service";
-import { normalizeElement } from "../services/textEditor.service";
-import { inline_elements } from "../constants/inline_elems";
-
-
+import DomUtilis from './DomUtilis';
+import { getStyle, normalizeStyle } from '../services/style.service';
+import { getClasses, normalizeClassName } from '../services/className.service';
+import { getAttributes } from '../services/attr.service';
+import { normalizeElement } from '../services/textEditor.service';
+import { inline_elements } from '../constants/inline_elems';
 
 /**
  *  * Returns the new merge element
@@ -13,189 +11,197 @@ import { inline_elements } from "../constants/inline_elems";
  */
 export const mergeNodeContect = node => {
 
-    if (!DomUtilis.isElement(node)) {
-        // console.error('one of the props is not dom element.., please insert two element to merge..');
-        return null;
-    }
-    if (!inline_elements[node.nodeName]   || node.nodeType !== Node.ELEMENT_NODE) {
-        // console.error('[mergeNodeContect] node is not a span');
-        return null;
-    }
-let commonTag = node.nodeName;
+  if (!DomUtilis.isElement(node)) {
+    // console.error('one of the props is not dom element.., please insert two element to merge..');
+    return null;
+  }
+  if (!inline_elements[node.nodeName] || node.nodeType !== Node.ELEMENT_NODE) {
+    // console.error('[mergeNodeContect] node is not a span');
+    return null;
+  }
+  let commonTag = node.nodeName;
 
-    const firstChild = node.firstElementChild;
-    if (!firstChild) {
-        // console.error('[mergeNodeContect] first child not found');
-        return null;
+  const firstChild = node.firstElementChild;
+  if (!firstChild) {
+    // console.error('[mergeNodeContect] first child not found');
+    return null;
+  }
+  if (firstChild.nodeName !== commonTag || firstChild.nodeType !== Node.ELEMENT_NODE) {
+    // console.error('[mergeNodeContect] firstChild is not a span');
+    return null;
+  }
+  if (node.textContent !== firstChild.textContent) {
+    // console.error('[mergeNodeContect] text content is different');
+    return null;
+  }
+  const _elementProps = {
+    style: getStyle(node),
+    classes: getClasses(node),
+    attrs: getAttributes(node)
+  };
+  for (const style in _elementProps.style) {
+    if (Object.prototype.hasOwnProperty.call(_elementProps.style, style)) {
+      const styleValue = _elementProps.style[style];
+      firstChild.style[style] = `${firstChild.style[style]} ${styleValue}`.trim();
     }
-    if (firstChild.nodeName !== commonTag || firstChild.nodeType !== Node.ELEMENT_NODE) {
-        // console.error('[mergeNodeContect] firstChild is not a span');
-        return null;
+
+  }
+  normalizeStyle(firstChild);
+  for (const attr in _elementProps.attrs) {
+    if (Object.prototype.hasOwnProperty.call(_elementProps.attrs, attr)) {
+      const attrValue = _elementProps.attrs[attr];
+      firstChild.setAttribute(attr, attrValue);
     }
-    if (node.textContent !== firstChild.textContent) {
-        // console.error('[mergeNodeContect] text content is different');
-        return null;
-    }
-    const _elementProps = {
-        style: getStyle(node),
-        classes: getClasses(node),
-        attrs: getAttributes(node)
-    }
-    for (const style in _elementProps.style) {
-        const styleValue = _elementProps.style[style];
-        firstChild.style[style] = (firstChild.style[style] + " " + styleValue).trim();
-    }
-    normalizeStyle(firstChild);
-    for (const attr in _elementProps.attrs) {
-        const attrValue = _elementProps.attrs[attr];
-            firstChild.setAttribute(attr, attrValue);
-    }
-    firstChild.classList.add(..._elementProps.classes);
-    normalizeClassName(firstChild);
-    normalizeElement(firstChild);
-    if(firstChild.parentElement)
-    firstChild.parentElement.unwrap();
-    return firstChild;
+
+  }
+  firstChild.classList.add(..._elementProps.classes);
+  normalizeClassName(firstChild);
+  normalizeElement(firstChild);
+  if (firstChild.parentElement) {firstChild.parentElement.unwrap();}
+  return firstChild;
 };
 export function mergeTwoNodes(elementOne, elementTwo) {
-    let tag;
-    if (!DomUtilis.isElement(elementOne) || !DomUtilis.isElement(elementTwo)) {
-        // console.error('one of the props is not dom element.., please insert two element to merge..');
-        return null;
-    }
-    if(!inline_elements[elementOne.nodeName]){
-        return null
-    }
-    tag = elementOne.nodeName;
-    if (elementOne.nodeName !== elementTwo.nodeName) {
-        // console.error('one of the props is not dom element.., please insert two element to merge..');
-        return null;
-    }
-    const _elementsData = [{
-        style: getStyle(elementOne),
-        classes: getClasses(elementOne),
-        attrs: getAttributes(elementOne)
-    },
-    {
-        style: getStyle(elementTwo),
-        classes: getClasses(elementTwo),
-        attrs: getAttributes(elementTwo)
-    }]
-    const getCommonClasses = (classes1, classes2) => {
-        if (!classes1 || !classes2)
-            return [];
-        const c1 = [...classes1];
-        const c2 = [...classes2];
-        return c1.filter(c => c2.includes(c));
-    }
-    const getCommonStyles = (style1, style2) => {
-        const commmonStyles = {};
-        for (const s in style1) {
-            if (!style2[s])
-                continue;
+  let tag;
+  if (!DomUtilis.isElement(elementOne) || !DomUtilis.isElement(elementTwo)) {
+    // console.error('one of the props is not dom element.., please insert two element to merge..');
+    return null;
+  }
+  if (!inline_elements[elementOne.nodeName]) {
+    return null;
+  }
+  tag = elementOne.nodeName;
+  if (elementOne.nodeName !== elementTwo.nodeName) {
+    // console.error('one of the props is not dom element.., please insert two element to merge..');
+    return null;
+  }
+  const _elementsData = [{
+    style: getStyle(elementOne),
+    classes: getClasses(elementOne),
+    attrs: getAttributes(elementOne)
+  },
+  {
+    style: getStyle(elementTwo),
+    classes: getClasses(elementTwo),
+    attrs: getAttributes(elementTwo)
+  }];
+  const getCommonClasses = (classes1, classes2) => {
+    if (!classes1 || !classes2) {return [];}
+    const c1 = [...classes1];
+    const c2 = [...classes2];
+    return c1.filter(c => c2.includes(c));
+  };
+  const getCommonStyles = (style1, style2) => {
+    const commmonStyles = {};
+    for (const s in style1) {
+      if (!style2[s]) {continue;}
 
-            const style1Values = style1[s];
-            const style2Values = style2[s];
-            if (style2Values === style1Values) {
-                commmonStyles[s] = style2Values;
-            }
-        }
-        return commmonStyles;
-    };
-    const collectedAttrs = {};
-    const isShouldBeWrapped = true;
-    for (const attr in _elementsData[0].attrs) {
-        const firstAttrValue = _elementsData[0].attrs[attr];
-        const SecAttrValue = _elementsData[1].attrs[attr];
-        if(firstAttrValue !== SecAttrValue){
-            isShouldBeWrapped = false;
-            break;
-        }
-        collectedAttrs[attr] = firstAttrValue;
-       
+      const style1Values = style1[s];
+      const style2Values = style2[s];
+      if (style2Values === style1Values) {
+        commmonStyles[s] = style2Values;
+      }
     }
-    if(!isShouldBeWrapped){
-        return null;
+    return commmonStyles;
+  };
+  const collectedAttrs = {};
+  let isShouldBeWrapped = true;
+  for (const attr in _elementsData[0].attrs) {
+    if (Object.prototype.hasOwnProperty.call(_elementsData[0].attrs, attr)) {
+      const firstAttrValue = _elementsData[0].attrs[attr];
+      const SecAttrValue = _elementsData[1].attrs[attr];
+      if (firstAttrValue !== SecAttrValue) {
+        isShouldBeWrapped = false;
+        break;
+      }
+      collectedAttrs[attr] = firstAttrValue;
     }
-    const commoncLasses = getCommonClasses(_elementsData[0].classes, _elementsData[1].classes);
-    const commonStyles = getCommonStyles(_elementsData[0].style, _elementsData[1].style);
-    const buildWrappingElement = (commonStyles, commoncLasses,collectedAttrs) => {
-        const wrapper = document.createElement(tag);
+  }
+  if (!isShouldBeWrapped) {
+    return null;
+  }
+  const commoncLasses = getCommonClasses(_elementsData[0].classes, _elementsData[1].classes);
+  const commonStyles = getCommonStyles(_elementsData[0].style, _elementsData[1].style);
+  const buildWrappingElement = (_commonStyles, _commoncLasses, _collectedAttrs) => {
+    const wrapper = document.createElement(tag);
 
-        for (const key in collectedAttrs) {
-            if (Object.hasOwnProperty.call(collectedAttrs, key)) {
-                const value = collectedAttrs[key];
-                wrapper.setAttribute(key,value);
-            }
-        }
-        // handle styles
-        for (const s in commonStyles)
-            wrapper.style[s] = commonStyles[s];
-            
+    for (const key in _collectedAttrs) {
+      if (Object.hasOwnProperty.call(_collectedAttrs, key)) {
+        const value = _collectedAttrs[key];
+        wrapper.setAttribute(key, value);
+      }
+    }
+    // handle styles
+    for (const s in _commonStyles) {
+      if (Object.prototype.hasOwnProperty.call(_commonStyles, s)) {
+        wrapper.style[s] = _commonStyles[s];
+      }
+    }
 
-        // handle classes
-        if (commoncLasses.length > 0) {
-            wrapper.classList.add(...commoncLasses);
-        }
-        if (wrapper.nodeName==="SPAN" &&wrapper.attributes.length === 0) {
-            wrapper.unwrap();
-            return null;
-        }else{
-            wrapper.normalize();
-        }
-        return wrapper;
+    // handle classes
+    if (_commoncLasses.length > 0) {
+      wrapper.classList.add(..._commoncLasses);
     }
-    const wrapper = buildWrappingElement(commonStyles, commoncLasses,collectedAttrs);
-    if (!wrapper) {
-        // console.error('wrapper is null')
-        return null;
+    if (wrapper.nodeName === 'SPAN' && wrapper.attributes.length === 0) {
+      wrapper.unwrap();
+      return null;
+    } else {
+      wrapper.normalize();
     }
-    const clearElementClasses = (node, commoncLasses) => {
-        node.classList.remove(...commoncLasses);
-        normalizeClassName(node);
-    }
-    const clearElementStyles = (node, commonStyles) => {
-        for (const s in commonStyles)
-            node.style[s] = null;
-    }
-    const clearAttrs = (node,attrs)=>{
-        for (const key in attrs) {
-            if (Object.hasOwnProperty.call(attrs, key)) {
-                node.removeAttribute(key);
-            }
-        }
-    }
-    const elements = [elementOne, elementTwo];
-    elements.forEach(e => {
-        clearElementClasses(e, commoncLasses);
-        clearElementStyles(e, commonStyles);
-        clearAttrs(e,collectedAttrs);
-    })
-
-    DomUtilis.wrap(elements, wrapper);
-    [...wrapper.children].forEach(c => {
-        normalizeElement(c)
-    })
     return wrapper;
-};
+  };
+  const wrapper = buildWrappingElement(commonStyles, commoncLasses, collectedAttrs);
+  if (!wrapper) {
+    // console.error('wrapper is null')
+    return null;
+  }
+  const clearElementClasses = (node, _commoncLasses) => {
+    node.classList.remove(..._commoncLasses);
+    normalizeClassName(node);
+  };
+  const clearElementStyles = (node, _commonStyles) => {
+    for (const s in _commonStyles) {
+      if (Object.prototype.hasOwnProperty.call(_commonStyles, s)) {
+        node.style[s] = null;
+      }
+    }
+  };
+  const clearAttrs = (node, attrs) => {
+    for (const key in attrs) {
+      if (Object.hasOwnProperty.call(attrs, key)) {
+        node.removeAttribute(key);
+      }
+    }
+  };
+  const elements = [elementOne, elementTwo];
+  elements.forEach(e => {
+    clearElementClasses(e, commoncLasses);
+    clearElementStyles(e, commonStyles);
+    clearAttrs(e, collectedAttrs);
+  });
 
+  DomUtilis.wrap(elements, wrapper);
+  [...wrapper.children].forEach(c => {
+    normalizeElement(c);
+  });
+  return wrapper;
+}
 
 export function mergeCommonValuesObject(obj1, obj2) {
-    let commonValues = {};
-    for (const key in obj1) {
-        if (obj2[key] && obj2[key] === obj1[key]) {
-            commonValues[key] = obj1[key]
-        }
+  let commonValues = {};
+  for (const key in obj1) {
+    if (obj2[key] && obj2[key] === obj1[key]) {
+      commonValues[key] = obj1[key];
     }
-    return commonValues;
+  }
+  return commonValues;
 }
 
 export function mergeCommonArrays(arr1, arr2) {
-    const mergeArr = [];
-    Array.from(arr1).forEach(item => {
-        if (arr2.findIndex(i => i === item) > 0) {
-            mergeArr.push(item);
-        }
-    })
-    return mergeArr;
+  const mergeArr = [];
+  Array.from(arr1).forEach(item => {
+    if (arr2.findIndex(i => i === item) > 0) {
+      mergeArr.push(item);
+    }
+  });
+  return mergeArr;
 }
